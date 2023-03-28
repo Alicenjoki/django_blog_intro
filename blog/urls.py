b25f3fb56ce2fd5c959fd7050699655c378fa498
Alice Kamuyu <alicekamuyu385@gmail.com>
2023-03-28 12:36:50 +0300

    added crude

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView,BlogCreateView,BlogUpdateView,BlogDeleteView
 
 urlpatterns =[
     path('', BlogListView.as_view(), name ='home'),
-    path('<int:pk>/', BlogDetailView.as_view(), name = 'detail' )
+    path('<int:pk>/', BlogDetailView.as_view(), name = 'detail' ),
+    path('new/', BlogCreateView.as_view(), name ='post_new' ),
+    path('new/<int:pk>/update/', BlogUpdateView.as_view(), name='post_update'),
+    path('new/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+
 ]
